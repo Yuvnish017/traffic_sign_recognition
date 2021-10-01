da92b5caff5064df95a5b6a391ce69d481a1b3e4
Yuvnish017 <malhotrayuvnish01@gmail.com>
2021-10-01 18:50:11 +0530

    something is coming up soon

--- TrafficSignRecognizer/visualization/visualization_tools.py
+++ TrafficSignRecognizer/visualization/visualization_tools.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sn
 from more_itertools import locate
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
 
 
 class Visualizer:
@@ -53,4 +56,57 @@ class Visualizer:
             plt.subplot(rows, cols, i)
             plt.imshow(self.x[idx[i]])
             plt.axis('off')
+        plt.show()
+
+    def predict_on_image(self, model, image):
+        try:
+            image = np.array(image)
+            if image.shape == 3:
+                image = np.expand_dims(image, axis=-1)
+            prediction = model.predict(image)
+            prediction = prediction.tolist()
+            m = max(prediction[0])
+            class_id = prediction[0].index(m)
+            figure = plt.figure(figsize=(5, 5))
+            plt.imshow(image)
+            plt.axis('off')
+            plt.title(class_id)
+            plt.show()
+        except:
+            print('Image shape inconsistent with model input shape')
+
+    def classification_report(self, model):
+        x = np.array(self.x)
+        y = np.array(self.y)
+        y = to_categorical(y)
+        y_pred = model.predict(x)
+        y_pred = np.argmax(y_pred, axis=1)
+        y = np.argmax(y, axis=1)
+        print(classification_report(y, y_pred))
+
+    def confusion_matrix(self, model):
+        x = np.array(self.x)
+        y = np.array(self.y)
+        y_pred = model.predict(x)
+        matrix = confusion_matrix(y, y_pred)
+        df = pd.DataFrame(matrix, index=list(set(self.y)), columns=list(set(self.y)))
+        figure = plt.figure(figsize=(10, 10))
+        sn.heatmap(df, annot=True, fmt='d')
+        plt.show()
+
+    def accuracy_and_loss_plots(self, model):
+        plt.plot(model.history.history['accuracy'], label='Train_accuracy')
+        plt.plot(model.history.history['val_accuracy'], label='Test_accuracy')
+        plt.title('Model Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend(loc="upper left")
+        plt.show()
+
+        plt.plot(model.history.history['loss'], label='Train_loss')
+        plt.plot(model.history.history['val_loss'], label='Test_loss')
+        plt.title('Model Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend(loc="upper left")
         plt.show()
